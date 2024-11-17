3f5e2850799b22c3a62b7c440daba9fb91bb8d5c
LondonDungeon <dokoeboi@gmail.com>
2024-11-18 01:54:49 +0300

    Updated main file

--- konfig/DZ_2/main.py
+++ konfig/DZ_2/main.py
@@ -3,18 +3,15 @@ import zipfile
 import io
 import graphviz
 import argparse
+from urllib.parse import urlparse
 
-
-# Get the dependencies of a package
 def get_dependencies(package_name):
-    # Set to store the dependencies
     dependencies = set()
     response = requests.get(f'https://pypi.org/pypi/{package_name}/json').json()
-
-    # If the package does not exist, return an empty set
+    
     if "message" in response:
         return dependencies
-
+    
     version = response["info"]["version"]
     releases = response["releases"]
     latest_release = releases[version][0]
@@ -22,7 +19,6 @@ def get_dependencies(package_name):
 
     WHLFile = requests.get(urlWHL)
 
-    # Skip if file name ends with tar.gz
     if urlWHL.endswith("tar.gz"):
         return dependencies
 
@@ -39,47 +35,82 @@ def get_dependencies(package_name):
             dependency = str(line).split(" ")
             if "extra" in dependency:
                 break
-            # Remove the version number
             dependency = dependency[1].split("\\")[0]
             dependencies.add(dependency)
     return dependencies
 
+def get_dependencies_github(repo_url, branch='main'):
+    dependencies = set()
+    try:
+        # Construct URL for raw requirements.txt
+        raw_url = repo_url.replace("github.com", "raw.githubusercontent.com") + f"/{branch}/requirements-dev.txt"
+        
+        # Get the content of the requirements.txt
+        response = requests.get(raw_url)
+        
+        if response.status_code != 200:
+            return dependencies
 
-# Format the dependencies to nested dictionaries
-def format_dependencies_to_nested_dicts(main_package, dependencies):
-    # Format: {main_package: [{dependency: [{dependency: []}]}]}
+        requirements_content = response.text
+        
+        # Parse dependencies
+        for line in requirements_content.splitlines():
+            line = line.strip()
+            if line and not line.startswith("#"):  # Skip empty lines and comments
+                dependencies.add(line.split("==")[0].strip())  # Get package name, ignore version
+
+    except Exception as e:
+        print(f"Error fetching dependencies: {e}")
+
+    return dependencies
+
+def format_dependencies_to_nested_dicts(main_package, dependencies, visited=None):
+    if visited is None:
+        visited = set()
+
     formatted_dependencies = {main_package: []}
 
     if dependencies is None:
         return formatted_dependencies
 
+    visited.add(main_package)
+
     for dependency in dependencies:
         dependency = dependency.split(" ")
-        # Skip if dependency is a link
-        if dependency == main_package:
+        package_name = dependency[0]
+
+        if package_name in visited:
             continue
-        if "extra" not in dependency:
-            package_name = dependency[0]
-            internal_dependencies = get_dependencies(package_name)
-            formatted_internal_dependencies = format_dependencies_to_nested_dicts(package_name, internal_dependencies)
-            formatted_dependencies[main_package].append(formatted_internal_dependencies)
+
+        visited.add(package_name)
+        internal_dependencies = get_dependencies(package_name)
+        formatted_internal_dependencies = format_dependencies_to_nested_dicts(
+            package_name, internal_dependencies, visited
+        )
+        formatted_dependencies[main_package].append(formatted_internal_dependencies)
+
     return formatted_dependencies
 
 
-# Convert the nested dictionaries to graph code
+
+
 def convertDicts(nested_dicts, depth, i):
     GraphCode = ""
+
     for key in nested_dicts:
         if not nested_dicts[key]:
             return f"\"{key}\";\n"
+        
         for nested_dict in nested_dicts[key]:
-            # If the depth is reached, return the key
-            if i >= depth:
-                if i + 1 < depth:
-                    return f"\"{key}\"->{convertDicts(nested_dict, depth, i + 1)}"
-                else:
-                    return f"\"{key}\"\n"
-            GraphCode += f"\"{key}\"->{convertDicts(nested_dict, depth, i + 1)}"
+            name = '"' + list(nested_dict.keys())[0].split('>')[0] + '";'
+            GraphCode += f"\"{key}\"->{name}\n"
+            
+            if i < depth:
+                dep = get_dependencies(list(nested_dict.keys())[0].split('>')[0])
+                dependency_tree = format_dependencies_to_nested_dicts(list(nested_dict.keys())[0].split('>')[0], dep)
+                links = convertDicts(dependency_tree, depth-i, 1)
+                GraphCode += links
+    
     return GraphCode
 
 def render_graph(dot_code, output_file):
@@ -87,29 +118,44 @@ def render_graph(dot_code, output_file):
     graph.render(output_file, format='png', cleanup=True)
     print(f"Graph saved as {output_file}.png")
 
-# Main function
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description="VM")
-    parser.add_argument('--graphviz', help="Path to the filesystem archive.")
-    parser.add_argument('--package', help="Path to the filesystem archive.")
-    parser.add_argument('--output', help="Path to the filesystem archive.")
-    parser.add_argument('--depth', help="Path to the filesystem archive.")
+    parser.add_argument('--graphviz')
+    parser.add_argument('--package')
+    parser.add_argument('--output')
+    parser.add_argument('--depth')
+    parser.add_argument('--url')
     args = parser.parse_args()
 
-    error_message = "Cannot get dependencies for this package"    
-    package_name = args.package
+    error_message = "Cannot get dependencies for this package"
+
     depth = int(args.depth)
     output = args.output
-
-    if len(package_name) < 3:
-        print(error_message)
-    else:        
-        dependencies = get_dependencies(package_name)
-        if dependencies:
-            dependency_tree = format_dependencies_to_nested_dicts(package_name, dependencies)
-            links = convertDicts(dependency_tree, depth, 0)
-            graph_code = "digraph G {\n" + links + "}"
-            #print(graph_code)
-            render_graph(graph_code, output)
-        else:
+    if args.package:
+        package_name = args.package
+        if len(package_name) < 3:
             print(error_message)
+        else:
+            dependencies = get_dependencies(package_name)
+            if dependencies:
+                dependency_tree = format_dependencies_to_nested_dicts(package_name, dependencies)
+                links = convertDicts(dependency_tree, depth, 1)
+                graph_code = "digraph G {\n" + links + "}"
+                render_graph(graph_code, output)
+    elif args.url:
+        package_url = args.url
+        package_name = urlparse(package_url).path.strip("/").split("/")[1]
+        if len(package_name) < 3:
+            print(error_message)
+        else:
+            dependencies = get_dependencies_github(package_url)
+            if dependencies:
+                dependency_tree = format_dependencies_to_nested_dicts(package_name, dependencies)
+                links = convertDicts(dependency_tree, depth, 1)
+                graph_code = "digraph G {\n" + links + "}"
+                render_graph(graph_code, output)
+    else:
+        print(error_message)
+
+if __name__ == '__main__':
+    main()
