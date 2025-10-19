c4081652c70eec71dc7892c7aaa7ff8950e3da42
sgolev <dokoeboi@gmail.com>
2025-10-19 21:09:19 +0300

    New monkeys

--- System_data_analysis/Practice6/monkey.py
+++ System_data_analysis/Practice6/monkey.py
@@ -15,7 +15,6 @@ class Monkey:
         self.yi = 0
         self.fi = 0
 
-        self.jumps = 0
         self.step = 0.1
 
     def calc(self):
@@ -23,82 +22,80 @@ class Monkey:
         return self.f
     
     def make_local_jump(self, max_jump):
-        self.xi = self.x
-        self.yi = self.y
-        self.fi = self.f
+        self.xi, self.yi,  self.fi = self.x, self.y, self.f
+        best_xi, best_yi, best_fi = self.xi, self.yi, self.fi
 
-        self.jumps = 0
-        while self.jumps != max_jump:
-            self.xi = np.random.uniform(self.xi - self.step, self.xi + self.step)
-            self.yi = np.random.uniform(self.yi - self.step, self.yi + self.step)
-            
-            self.xi = np.clip(self.xi, -4.5, 4.5)
-            self.yi = np.clip(self.yi, -4.5, 4.5)
+        jumps_left = max_jump
 
-            self.jumps += 1
-        
-        self.fi = f(self.xi, self.yi)
+        while jumps_left > 0:
+            trial_x = np.random.uniform(self.xi - self.step, self.xi + self.step)
+            trial_y = np.random.uniform(self.yi - self.step, self.yi + self.step)
+
+            trial_x = np.clip(trial_x, -4.5, 4.5)
+            trial_y = np.clip(trial_y, -4.5, 4.5)
+
+            trial_f = f(trial_x, trial_y)
+
+            if trial_f < best_fi:
+                best_xi, best_yi, best_fi = trial_x, trial_y, trial_f
+                jumps_left = max_jump
+            else:
+                jumps_left -= 1
+
+        self.xi, self.yi, self.fi = best_xi, best_yi, best_fi
         return self.fi
-    
+
     def make_global_jump(self, c_x, c_y):
-            self.x = self.x + np.random.uniform(1, 2) * (c_x - self.x)
-            self.y = self.y + np.random.uniform(1, 2) * (c_y - self.y)
+        self.x = self.x + np.random.uniform(1, 2) * (c_x - self.x)
+        self.y = self.y + np.random.uniform(1, 2) * (c_y - self.y)
 
-            self.x = np.clip(self.x, -4.5, 4.5)
-            self.y = np.clip(self.y, -4.5, 4.5)
+        self.x = np.clip(self.x, -4.5, 4.5)
+        self.y = np.clip(self.y, -4.5, 4.5)
 
-            self.calc()
+        self.calc()
     
     def update(self):
-        self.x = self.xi
-        self.y = self.yi
-        self.f = self.fi
+        self.x, self.y, self.f = self.xi, self.yi, self.fi
 
 
 class Troop:
     def __init__(self):
-        self.monkeys = 10
-        self.jumps = 5
-
-        self.c_x = 0
-        self.c_y = 0
+        self.n_monkeys = 15
+        self.max_local_jumps = 20
 
     def monkey_init(self, minZ, maxZ):
         monkeyz = []
-        for i in range (self.monkeys):
-            monk = Monkey()
-            monk.x = np.random.uniform(minZ, maxZ)
-            monk.y = np.random.uniform(minZ, maxZ)
-            monk.calc()
-
-            monkeyz.append(monk)
-        
+        for _ in range(self.n_monkeys):
+            m = Monkey()
+            m.x = np.random.uniform(minZ, maxZ)
+            m.y = np.random.uniform(minZ, maxZ)
+            m.calc()
+            
+            monkeyz.append(m)
         return monkeyz
-
 
     def find_best(self, iter):
         monkeyz = self.monkey_init(-4.5, 4.5)
 
         for i in range(iter):
-            self.c_x = sum(m.x for m in monkeyz) / len(monkeyz)
-            self.c_y = sum(m.y for m in monkeyz) / len(monkeyz)
-            
+            c_x = np.mean([m.x for m in monkeyz])
+            c_y = np.mean([m.y for m in monkeyz])
+
             for monk in monkeyz:
-                if monk.make_local_jump(self.jumps) < monk.f:
+                if monk.make_local_jump(self.max_local_jumps) < monk.f:
                     monk.update()
                 else:
-                    monk.make_global_jump(self.c_x, self.c_y)
+                    monk.make_global_jump(c_x, c_y)
 
         monkeyz.sort(key=lambda b: b.f)
         return monkeyz[0]
-
-
 
 
 if __name__ == '__main__':
     print("Эталон: f(3, 0.5) = 0")
 
     troop = Troop()
-    iter = 1000
-    best_monk = troop.find_best(iter)
-    print(f"Результат x = {best_monk.x} y = {best_monk.y} f = {best_monk.f}")
+    iter = 100
+
+    best_monkey = troop.find_best(iter)
+    print(f"Результат x = {best_monkey.x:.5f} y = {best_monkey.y:.5f} f = {best_monkey.f:.8f}")
